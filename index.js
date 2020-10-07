cc6cdb9c53a7ddd8f4e52aa611cb21d74279d7b7
EC2 Default User <ec2-user@ip-172-31-4-68.us-east-2.compute.internal>
2020-10-07 23:55:06 +0000

    started add function

---
+++ index.js
@@ -0,0 +1,38 @@
+let carts = document.querySelectorAll('.add-cart');
+
+let products = [
+    {
+        
+    }
+]
+
+for(let i=0; i< carts.length; i++){
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+    })
+}
+
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    if(productNumbers){
+         document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    productNumbers = parseInt(productNumbers);
+    
+    if(productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    }else{
+        localStorage.setItem('cartNumbers',1);
+        document.querySelector('.cart span').textContent =1;
+    }
+    
+    
+}
+onLoadCartNumbers();
